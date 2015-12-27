ad5bd6d71068675f4b074ad3fbdddfb5111cf05d
ggu <gabriel.uribe@alumni.ubc.ca>
2015-12-27 16:39:16 -0500

    Added Manhattan and Diagonal distance formulas to apply to Coordinates

--- SimpleGridView/logic/Coordinate.swift
+++ SimpleGridView/logic/Coordinate.swift
@@ -11,4 +11,16 @@ struct Coordinate
 {
   var x : Int
   var y : Int
+  
+  // Manhattan distance
+  func mDistance(coordinate: Coordinate) -> Int
+  {
+    return abs(x - coordinate.x) + abs(y - coordinate.y)
+  }
+  
+  // Diagonal distance
+  func dDistance(coordinate: Coordinate) -> Int
+  {
+    return max(abs(x - coordinate.x), abs(y - coordinate.y))
+  }
 }
